797cd0035d76dde02c8b419f120fa77ff42a0b4b
assendk <assendk@gmail.com>
2017-11-02 02:44:09 +0200

    footer css

--- view/footer.php
+++ view/footer.php
@@ -8,9 +8,8 @@
 <div class="container background-dark-gray">
     <div class="row">
         <div class="col-lg-12">
-            <div class="col-md-3">email: admin@s8ftracker.tk</div>
-            <div class="col-md-3">in the net</div>
-            <div class="col-md-3">$8 finance tracker 2017</div>
+            <div class="col-md-6"><h4>$8 finance tracker 2017</h4></div>
+            <div class="col-md-6"><h5 class="pull-right">contact: admin@s8ftracker.tk</h5></div>
             <br><br>
         </div>
     </div>
--- view/overview.php
+++ view/overview.php
@@ -34,7 +34,6 @@ include "../model/userClass.php";
     <!--    <script src="js/raphael.min.js"></script>-->
     <!--    <script src="js/morris.min.js"></script>-->
     <!--    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">-->
-<!--    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>-->
     <!--    <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>-->
     <!--    <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>-->
     <script src="js/raphael212-min.js"></script>
@@ -57,7 +56,7 @@ include "../model/userClass.php";
 
     <div class="row">
         <?php
-        $categories_exp_json_data = $incomes_cat_json_data = "";
+        $categories_exp_json_data = $incomes_cat_json_data = $categories_exp_json_data = "";
         include "../controller/chart_get_categories.php";
         include "../controller/chart_get_income_categories.php";
         include "../controller/chart_morris_exp_inc.php";
@@ -65,14 +64,15 @@ include "../model/userClass.php";
         ?>
         <div class="col-md-4">
             <div id="donut-categories" style="height: 250px;"></div>
-            <div>2017 Expenses Categories</div>
+            <div class="row text-center">2017 Expenses Categories</div>
         </div>
         <div class="col-md-4">
             <div id="donut-inc-acc" style="height: 250px;"></div>
-            <div class="">2017 Incomes</div>
+            <div class="row text-center">2017 Incomes</div>
         </div>
         <div class="col-md-4">
             <div id="donut-exp-inc" style="height: 250px;"></div>
+            <div class="row text-center">2017 Balance</div>
         </div>
     </div>
     <div class="col-md-12">
